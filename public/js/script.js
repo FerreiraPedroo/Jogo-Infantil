54b8debe7d462d619f1d6791e28d9bf505015019
Layssa <layssa.lima166@gmail.com>
2021-08-04 14:34:18 -0300

    update game

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,3 @@
-// $(document).ready(() => {
-//     $(function () {
-//         $(".draggable").draggable();
-//         $("#droppable").droppable({
-//             drop: function (event, ui) {
-//                 $(this)
-//                     .addClass("ui-state-highlight")
-//                     .find(".letters-words-none")
-//                     .html("Dropped!");
-//             }
-//         });
-//     });
-
-// });
-
 class DragDrop {
     #wordList;
 
@@ -35,22 +20,25 @@ class DragDrop {
 
 }
 
+// variavel de pontuação
+let punctuation = 0;
+const words = ["abelha", "navio", "vaca"];
+const feedbackGame = new Audio("https://audio-previews.elements.envatousercontent.com/files/173295841/preview.mp3?response-content-disposition=attachment%3B+filename%3D%22HL56S7T-bonus-points.mp3%22")
 
-
-// a função recebe pelo parametro um array com a quantidade de palaras que será inseridas no html.
+//------------- a função recebe pelo parametro um array com a quantidade de palaras que será inseridas no html. -------------
 function createWordList(_word) {
-    // recebe o array com as palavras.
+    // ------------- recebe o array com as palavras. -------------
     const wordList = _word;
 
     wordList.forEach(element => {
-        //cria o html da linha da palavra
-        //variavel que armazenará o html que será inserido na página.
+        //------------- cria o html da linha da palavra -------------
+        // ------------- variavel que armazenará o html que será inserido na página.-------------
         let wordHTML = `
         <div class="words">
-              <div class="letters-words ui-widget-header drop"></div>
+              <div class="letters-words ui-widget-header drop" id="${element}"></div>
         `;
         for (let w = 1; w < element.length; w++) {
-            // cria o html das letras da palavra
+            // ------------- cria o html das letras da palavra -------------
             wordHTML += `
                 <div class="letters-words">
                             <img src="./images/letter-${element[w]}.png" alt="">
@@ -59,22 +47,39 @@ function createWordList(_word) {
         }
         wordHTML += `<img class="object" src="./images/${element}.png" alt=""></img>`;
         wordHTML += `</div>`;
-        // inseri a palavra no html.
+        //------------- insere a palavra no html. -------------
         $("#words-space").append(wordHTML);
     });
 }
 
-
-createWordList(["abelha", "navio", "vaca"]);
-
+createWordList(words);
 
 $(".drag").draggable({
     cursor: "grabbing",
     revert: "invalid",
     snap: ".drop",
     snapMode: "inner",
-})
+    snapTolerance: 40
+});
 
+// const point = new Audio("https://audio-previews.elements.envatousercontent.com/files/151063428/preview.mp3?response-content-disposition=attachment%3B+filename%3D%22NW8FSR4-scoring-a-point.mp3%22");
 $(".drop").droppable({
+    // evento que ocorre quando o drag é arrastado
+    activate: function (event, ui) {
+        // $(this).css("border","1px solid #DCC706")
+    },
+    // evento que ocorre quando o drag é solto no drop
+    drop: function (event, ui) {
+        $(this).css("background", "#728C0B");
+        const point = new Audio("https://audio-previews.elements.envatousercontent.com/files/151063428/preview.mp3?response-content-disposition=attachment%3B+filename%3D%22NW8FSR4-scoring-a-point.mp3%22");
+        punctuation++
+        if (punctuation >= 3) {
+            feedbackGame.play()
+            return
+        } else {
+            point.play();
+            console.log(punctuation);
+        }
+    }
+});
 
-})
