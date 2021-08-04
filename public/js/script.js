a134495f48bc86f91745b504d73c8475bfb8f203
Layssa <layssa.lima166@gmail.com>
2021-08-03 23:44:11 -0300

    update game

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,14 @@
-$(document).ready(()=>{
-    
-})
+$(document).ready(() => {
+    $(function () {
+        $(".draggable").draggable();
+        $("#droppable").droppable({
+            drop: function (event, ui) {
+                $(this)
+                    .addClass("ui-state-highlight")
+                    .find(".letters-words-none")
+                    .html("Dropped!");
+            }
+        });
+    });
+
+});
