78b08fb8ac58c2821da0a6f6058086fa50cd3eec
Layssa <layssa.lima166@gmail.com>
2021-08-04 17:29:51 -0300

    update musics game

--- public/js/script.js
+++ public/js/script.js
@@ -21,13 +21,14 @@ class DragDrop {
 // variavel de pontuação
 let score = 0;
 const words = ["abelha", "navio", "vaca"];
+const musicGame = new Audio("https://audio-previews.elements.envatousercontent.com/files/109551272/preview.mp3?response-content-disposition=attachment%3B+filename%3D%22XPT5HRY-video-game.mp3%22");
 const feedbackGame = new Audio("https://audio-previews.elements.envatousercontent.com/files/173295841/preview.mp3?response-content-disposition=attachment%3B+filename%3D%22HL56S7T-bonus-points.mp3%22")
 
 //------------- a função recebe pelo parametro um array com a quantidade de palaras que será inseridas no html. -------------
 function createWordList(_word) {
     // ------------- recebe o array com as palavras. -------------
     const wordList = _word;
-
+    musicGame.play()
     wordList.forEach(element => {
         //------------- cria o html da linha da palavra -------------
         // ------------- variavel que armazenará o html que será inserido na página.-------------
@@ -69,6 +70,11 @@ $(".drop").droppable({
     // evento que ocorre quando o drag é solto no drop
     drop: function (event, ui) {
         $(this).css("background", "#728C0B");
+        $(this).droppable({
+            disabled : true
+        })
+        // $(this).draggable("disable")
+
         const pointAudio = new Audio("https://audio-previews.elements.envatousercontent.com/files/151063428/preview.mp3?response-content-disposition=attachment%3B+filename%3D%22NW8FSR4-scoring-a-point.mp3%22");
         score++;
         if (score >= 3) {
