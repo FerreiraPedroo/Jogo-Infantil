c8536b0835f4e19d3b3f69386e3e43def1fcfefb
Layssa <layssa.lima166@gmail.com>
2021-08-04 21:04:18 -0300

    update game

--- public/js/script.js
+++ public/js/script.js
@@ -22,7 +22,6 @@ class DragDrop {
 let score = 0;
 const words = ["abelha", "navio", "vaca"];
 const musicGame = new Audio("./music/theme-music.mp3");
-
 const feedbackGame = new Audio("https://audio-previews.elements.envatousercontent.com/files/173295841/preview.mp3?response-content-disposition=attachment%3B+filename%3D%22HL56S7T-bonus-points.mp3%22")
 
 //------------- a função recebe pelo parametro um array com a quantidade de palaras que será inseridas no html. -------------
@@ -62,35 +61,63 @@ $(".drag").draggable({
     snapTolerance: 40
 });
 
-words.forEach((elements)=>{
+words.forEach((elements) => {
     console.log(elements);
     $(`.drop_${elements}`).droppable({
-            accept: `.${elements}`,
-            // evento que ocorre quando o drag é arrastado
-            activate: function (event, ui) {
-                // $(this).css("border","1px solid #DCC706")
-            },
-            // evento que ocorre quando o drag é solto no drop
-            drop: function (event, ui) {
-                $(this).css("background", "#728C0B");
-                $(this).droppable({
-                    disabled : true
-                })
-        
-                const pointAudio = new Audio("https://audio-previews.elements.envatousercontent.com/files/151063428/preview.mp3?response-content-disposition=attachment%3B+filename%3D%22NW8FSR4-scoring-a-point.mp3%22");
-                score++;
-                if (score >= 3) {
-                    feedbackGame.play();
-                    return;
-                } else {
-                    pointAudio.play();
-                }
+        accept: `.${elements}`,
+        // evento que ocorre quando o drag é arrastado
+        activate: function (event, ui) {
+            // $(this).css("border","1px solid #DCC706")
+        },
+        // evento que ocorre quando o drag é solto no drop
+        drop: function (event, ui) {
+            $(this).css("background", "#728C0B");
+            $(this).droppable({
+                disabled: true
+            })
+
+            const pointAudio = new Audio("https://audio-previews.elements.envatousercontent.com/files/151063428/preview.mp3?response-content-disposition=attachment%3B+filename%3D%22NW8FSR4-scoring-a-point.mp3%22");
+            score += 30;
+            if (score >= 90) {
+                score = 100;
+                feedbackGame.play();
+                scoreUpdate();
+                victory();
+                return;
+            } else {
+                pointAudio.play();
+                scoreUpdate()
             }
-        });
+        }
+    });
 });
 
-$( function() {
-    $( "#progressbar" ).progressbar({
-      value: 50
+function scoreUpdate() {
+    $(function () {
+        $("#progressbar").progressbar({
+            value: score
+        });
     });
-  } );
+}
+
+function victory() {
+    $("body").append(`
+    <div id="feedback-user">
+        <div id="result">
+            <p>VITÓRIA!!!</p>
+             <div id="stars">
+                <img src="./images/star.png" width="80px" alt="">
+                <img src="./images/star.png" width="80px" alt="">
+                 <img src="./images/star.png" width="80px" alt="">
+         </div>
+         <button id="next">Próxima Fase</button>
+ </div>
+ <img id="character-feedback" src="./images/character.png" alt="">
+</div>
+    `);
+
+    $("#character").css("display", "none");
+}
+
+scoreUpdate()
+
