47a6803d6c537b3572ef7c25c92b3bd3c3a4696c
Pedro Henrique de Assis Ferreira <73146680+Simplerino@users.noreply.github.com>
2021-08-06 19:52:28 -0300

    update game

--- public/js/letter-script-class.js
+++ public/js/letter-script-class.js
@@ -31,19 +31,19 @@ export class DragDropClass {
             if (this.#letterListSelected[randomLetterPosition] == undefined && randomLetterQtd < this.#wordListQtd) {
                 this.#letterListSelected[randomLetterPosition] = this.#wordListSelected[randomLetterQtd][0];
                 randomLetterQtd++
-                console.log("(3 <) Pos: ", randomLetterPosition, "LS: ", this.#letterListSelected[randomLetterPosition])
+                console.log("(3 <) Pos: ", randomLetterPosition, "LS: ", this.#letterListSelected[randomLetterPosition]);
             }
             if (this.#letterListSelected[randomLetterPosition] == undefined && randomLetterQtd >= this.#wordListQtd) {
                 let letterSelected = this.#letterAlphabet[Math.floor(Math.random() * this.#letterAlphabet.length)];
                 if (this.#letterListSelected.indexOf(letterSelected) == -1) {
                     this.#letterListSelected[randomLetterPosition] = letterSelected;
                     randomLetterQtd++
-                    console.log("(3 >=) Pos: ", randomLetterPosition, "LS: ", this.#letterListSelected[randomLetterPosition])
+                    console.log("(3 >=) Pos: ", randomLetterPosition, "LS: ", this.#letterListSelected[randomLetterPosition]);
                 }
             }
         }
         while (randomLetterQtd < this.#letterTotal);
-        return this.#letterListSelected
+        return this.#letterListSelected;
     }
     // return random wordlist(Array), no repeat word
     randomWordListSelect() {
@@ -54,7 +54,7 @@ export class DragDropClass {
             randomWordPosition = Math.floor(Math.random() * this.#wordListQtd);
             if (this.#wordListSelected[randomWordPosition] == undefined) {
                 let wordSelected = this.#wordList[Math.floor(Math.random() * this.#wordList.length)];
-                console.log("Pos: ", randomWordQtd, "WS: ", wordSelected)
+                console.log("Pos: ", randomWordQtd, "WS: ", wordSelected);
 
                 if (this.#wordListSelected.indexOf(wordSelected) == -1) {
                     this.#wordListSelected[randomWordPosition] = wordSelected;
@@ -64,14 +64,15 @@ export class DragDropClass {
             }
         }
         while (randomWordQtd < this.#wordListQtd);
-        return this.#wordListSelected
+        return this.#wordListSelected;
     }
 
     // set list of words, receive "Array"
     set setWordList(_wordList) {
         if (Array.isArray(_wordList)) {
             this.#wordList = _wordList;
-            //console.log("SetWordList: ", this.#wordList);
+            console.log("SetWordList: ", this.#wordList);
+            console.log("SetWordListLength: ", this.#wordList.length);
         } else {
             return "erro: parametro deve ser um array";
         }
@@ -86,14 +87,16 @@ export class DragDropClass {
     set setExtraLetter(_letterExtraValue) {
         if (Number.isInteger(_letterExtraValue)) {
             this.#letterExtra = _letterExtraValue;
-            //console.log("SetExtraNumber: ", this.#letterExtra );
+            console.log("SetExtraNumber: ", this.#letterExtra );
         } else {
             return "erro: parametro deve ser um 'Number'";
         }
     }
 
     getWordListSelected(_qtd) {
-        this.#wordListQtd = _qtd;
+        //this.#wordListQtd = _qtd;
+        this.#wordListQtd = this.#wordList.length > _qtd ? _qtd : this.#wordList.length;
+        console.log(this.#wordListQtd);
         this.#letterTotal = (this.#wordListQtd + this.#letterExtra) <= this.#letterAlphabet.length ? (this.#wordListQtd + this.#letterExtra) : 25;
         this.randomWordListSelect();
         this.randomLetterList();
